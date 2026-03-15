cd5e396e5cc99c4a9ec4888bcc3ee9e13be5d667
Zack289 <ozack911@gmail.com>
2026-03-15 23:38:45 +0545

    Updating the loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import styles from "./Loader.module.css";
 export default function Loader() {
   return (
     <div className={styles.loaderWrapper}>
-      <div className={styles.loader}></div>
+      <div className={styles.spinner}></div>
     </div>
   );
 }
